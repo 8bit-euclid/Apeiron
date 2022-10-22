b2ca1753271785d359bd15b5f660ae725ab00e82
Byte-Sized Euclid <8bit.euclid@gmail.com>
2022-10-22 15:06:14 +0200

    Cleaned up and commented line shader.

--- libs/Visualiser/resources/shaders/Line.glsl
+++ libs/Visualiser/resources/shaders/Line.glsl
@@ -1,55 +1,69 @@
 #shader vertex
 #version 460 core
 
-layout(std430, binding = 0) buffer TVertex
-{
-   vec4 vertex[];
-};
+layout(std430, binding = 0) buffer TVertex { vec4 vertex[]; };
 
-uniform mat4 u_model_matrix;
-uniform mat4 u_view_matrix;
-uniform mat4 u_projection_matrix;
-//uniform mat4  u_mvp;
+uniform mat4  u_mvp_matrix; // proj * view * model
 uniform vec2  u_resolution;
 uniform float u_thickness;
 
 void main()
 {
-   int line_i = gl_VertexID / 6;
-   int tri_i  = gl_VertexID % 6;
+   /**
+      0 3   5
+      o o---o
+      |\ \  |
+      | \ \ |
+      |  \ \|
+      o---o o
+      1   2 4
+   */
+   int line_id = gl_VertexID / 6; // Line segment index (each segment has 6 vertices).
+   int vert_id = gl_VertexID % 6; // Vertex index in each triangle (indices 0 - 5).
 
-   vec4 va[4];
-   for (int i=0; i<4; ++i)
+   // The coordinates have to be transformed from model space to window space.
+   vec4 vertices[4]; // Four vertices belonging to a string of 3 line segments.
+   for(int i = 0; i < 4; ++i)
    {
-       va[i] = u_projection_matrix * u_view_matrix * u_model_matrix * vertex[line_i + i];
-       va[i].xyz /= va[i].w;
-       va[i].xy = (va[i].xy + 1.0) * 0.5 * u_resolution;
+       vertices[i]      = u_mvp_matrix * vertex[line_id + i];
+       vertices[i].xyz /= vertices[i].w; // Perspective divide
+       vertices[i].xy   = (vertices[i].xy + 1.0) * 0.5 * u_resolution;
    }
 
-   vec2 v_line  = normalize(va[2].xy - va[1].xy);
-   vec2 nv_line = vec2(-v_line.y, v_line.x);
+   //   0            1            2            3
+   //   o------------o------------o------------o
+   //        prev         curr         next
+   const vec2 tangent_curr = normalize(vertices[2].xy - vertices[1].xy);
+   const vec2 normal_curr  = vec2(-tangent_curr.y, tangent_curr.x);
 
-   vec4 pos;
-   if (tri_i == 0 || tri_i == 1 || tri_i == 3)
+   vec4 position;
+   if(vert_id == 0 || vert_id == 1 || vert_id == 3)
    {
-      vec2 v_pred  = normalize(va[1].xy - va[0].xy);
-      vec2 v_miter = normalize(nv_line + vec2(-v_pred.y, v_pred.x));
+      const vec2  tangent_prev = normalize(vertices[1].xy - vertices[0].xy);
+      const vec2  normal_prev  = vec2(-tangent_prev.y, tangent_prev.x);
+      const vec2  miter_1      = normalize(normal_curr + normal_prev);
+      const float offset       = (vert_id == 1 ? -0.5 : 0.5) * u_thickness / dot(miter_1, normal_curr);
 
-      pos = va[1];
-      pos.xy += v_miter * u_thickness * (tri_i == 1 ? -0.5 : 0.5) / dot(v_miter, nv_line);
+      position     = vertices[1];
+      position.xy += offset * miter_1;
    }
-   else
+   else // vert_id == 2 || vert_id == 4 || vert_id == 5
    {
-      vec2 v_succ  = normalize(va[3].xy - va[2].xy);
-      vec2 v_miter = normalize(nv_line + vec2(-v_succ.y, v_succ.x));
+      const vec2  tangent_next = normalize(vertices[3].xy - vertices[2].xy);
+      const vec2  normal_next  = vec2(-tangent_next.y, tangent_next.x);
+      const vec2  miter_2      = normalize(normal_curr + normal_next);
+      const float offset       = (vert_id == 5 ? 0.5 : -0.5) * u_thickness / dot(miter_2, normal_curr);
 
-      pos = va[2];
-      pos.xy += v_miter * u_thickness * (tri_i == 5 ? 0.5 : -0.5) / dot(v_miter, nv_line);
+      position     = vertices[2];
+      position.xy += offset * miter_2;
    }
 
-   pos.xy = pos.xy / u_resolution * 2.0 - 1.0;
-   pos.xyz *= pos.w;
-   gl_Position = pos;
+   // Finally the window coordinates must be transformed back to clip space. Transform from window space to normalized device space.
+   // The perspective divide must also be reversed.
+   position.xy   = position.xy / u_resolution * 2.0 - 1.0;
+   position.xyz *= position.w;
+
+   gl_Position = position;
 }
 
 
@@ -57,9 +71,9 @@ void main()
 #shader fragment
 #version 460 core
 
-out vec4 fragColor;
+layout(location = 0) out vec4 fragment_colour;
 
 void main()
 {
-   fragColor = vec4(1.0);
+   fragment_colour = vec4(1.0);
 }
